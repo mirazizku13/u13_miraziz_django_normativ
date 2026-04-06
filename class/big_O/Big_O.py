8abf10b0b549a8dad4c57f16377b4c085b290680
mirazizku13 <mirazizmirabdulayev11@gmail.com>
2026-04-06 10:53:46 +0500

    Delete class directory

--- class/big_O/Big_O.py
+++
@@ -1,5 +0,0 @@
-a = [1,2,10,12]
-b = 0
-for i in a :
-     b+=i
-print(b)
--- class/les19.py
+++
@@ -1,7 +0,0 @@
-class Animal:
-    def __init__(self, name, age, rangi):
-        self.name = name
-        self.age = age
-        self.rangi = rangi
-    def get_info(self):
-        return f'nomi {self.name}, yoshi {self.age}, rangi{self.rangi}'
